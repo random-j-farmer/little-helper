bfafa94b07e1f23081760eb5c1a16c4b9b7514ce
Random J. Farmer <random.j.farmer@gmail.com>
2016-02-21 13:12:36 +0100

    probe item categories

--- app/jvm/src/test/resources/item_types.sql
+++ app/jvm/src/test/resources/item_types.sql
@@ -1,2 +1,8 @@
-select it.typeName, ig.groupName, ig.categoryID, ic.categoryName from invTypes it, invGroups ig, invCategories ic where ig.groupID=it.groupID and ig.categoryID=ic.categoryID and ig.categoryID in (2,3,6,18,22,23,25,40,46,65);
+/*
+8 is for charges (scanner probes!)
+*/
+select it.typeName, ig.groupName, ig.categoryID, ic.categoryName 
+from invTypes it, invGroups ig, invCategories ic 
+where ig.groupID=it.groupID and ig.categoryID=ic.categoryID 
+and ig.categoryID in (2,3,6,8,18,22,23,25,40,46,65);
 
